2fe7b50fd925b40f617edbb636bb901843956b6c
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2026-02-22 19:04:15 -0300

    Adicionei o tela de carregamento

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import LoadingScreen from '@/components/ui/LoadingScreen';
 
 export const Route = createRootRoute({
   component: () => {
     return (
       <div className="min-h-screen">
+        <LoadingScreen />
         <Outlet />
       </div>
     );
