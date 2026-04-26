4bc3ce0dc7d72a32608d01169b8d8e4d34ad3b1b
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2026-04-26 13:39:29 -0300

    feat: manage loading state globally in root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,20 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { useState } from 'react';
 import LoadingScreen from '../components/LoadingScreen';
 
 export const Route = createRootRoute({
-  component: () => {
-    return (
-      <div className="min-h-screen">
-        <LoadingScreen />
-        <Outlet />
-      </div>
-    );
-  },
+  component: RootComponent,
 });
+
+function RootComponent() {
+  const [showLoader, setShowLoader] = useState(true);
+
+  return (
+    <div className="min-h-screen bg-[#8B00FF]">
+      {showLoader && (
+        <LoadingScreen onFinished={() => setShowLoader(false)} />
+      )}
+      <Outlet />
+    </div>
+  );
+}
